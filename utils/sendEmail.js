9a0c7801fa5db5ace7b3f35b0f571f90b6198d90
Muhamamd Ali <mali3473550895@gmail.com>
2026-04-09 18:32:21 +0500

    fixed some issues on email sent

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,14 +3,19 @@ import logger from './logger.js';
 
 const sendEmail = async (options) => {
   try {
+    const port = Number(process.env.EMAIL_PORT) || 587;
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: process.env.EMAIL_PORT === '465',
+      port,
+      secure: port === 465,
+      requireTLS: port === 587,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      connectionTimeout: 60_000,
+      greetingTimeout: 30_000,
+      socketTimeout: 60_000,
     });
 
     const mailOptions = {
